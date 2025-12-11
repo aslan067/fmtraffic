7828fd42060d432da656e73c44b03afcdef98e0a
faslan <40107696+aslan067@users.noreply.github.com>
2025-12-11 17:51:50 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 
+module.exports = {
+  output: 'export',        // GitHub Pages uyumlu statik export
+  trailingSlash: true,     // /en/ -> /en/index.html üretir
+  images: { unoptimized: true }, // export modunda
+  experimental: {
+    optimizePackageImports: ['react', 'react-dom']
+  }
+};
